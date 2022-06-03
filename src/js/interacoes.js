19ac2bed0e28e39bce8921ebc6baab26b33492d5
IsmaelSS <ismael337sn@gmail.com>
2022-06-03 09:33:23 -0300

    primeira versão do comitada

---
+++ src/js/interacoes.js
@@ -0,0 +1,10 @@
+const botaoQSP = document.querySelector('#header__areaBotao')
+const url = 'https://inscricao.kenzie.com.br/'
+
+function abrirUrlQSP(){
+    window.open(url,'_blank')
+}
+
+function acender(){
+    
+}
